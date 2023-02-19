4f2965db090f6ec466a5a22fd0ba5a9d5f3d1cf6
Craig Lowdon <craiglowdon@hotmail.com>
2023-02-19 12:57:25 +0000

    new func, prints cli args

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Entry point")
-	HelloWorld()
+	list()
 }
 
 func HelloWorld() string {
 	return "Hello World!"
+}
+
+func list() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
