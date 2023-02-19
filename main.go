8f006450bc19e6be92f41207acf52faf9d50b397
Craig <101149111+Craiglowdon@users.noreply.github.com>
2023-02-19 13:43:40 +0000

    Dev (#1)
    * new func, prints cli args
    
    * optimised for loop
    
    * squashed changes

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Entry point")
-	HelloWorld()
+	listArgs()
 }
 
 func HelloWorld() string {
 	return "Hello World!"
+}
+
+func listArgs() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
 }
